308577009d7a22bebae9acd520cb4ab4c8b40114
buryk20 <73736907+buryk20@users.noreply.github.com>
2022-03-10 23:09:35 +0200

    advens

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 // import App from './App.vue'
 import range from './components/range.vue'
 import sliderAdvent from './components/sliderAdvant.vue'
+import cases from './components/cases.vue'
 
 // createApp(App).mount('#app')
 createApp(range).mount('#range')
 createApp(sliderAdvent).mount('#sliderAdvent')
+createApp(cases).mount('#cases')
